75a70cf2301ef85bafd8bfa7f411dc983a77a673
crystalDf <chendong333@gmail.com>
2016-08-12 23:24:19 +0800

    Chapter 12 Excel

--- excel_test.py
+++ excel_test.py
@@ -15,3 +15,18 @@ print(sheet.title)
 another_sheet = wb.active
 
 print(another_sheet)
+
+sheet = wb.get_sheet_by_name('Sheet1')
+
+print(sheet['A1'])
+
+print(sheet['A1'].value)
+
+cell = sheet['B1']
+
+print(cell.value)
+
+print('Row' + str(cell.row) + ', Column ' + cell.column + ' is ' + cell.value)
+print('Cell ' + cell.coordinate + ' is ' + cell.value)
+
+print(sheet['C1'].value)
