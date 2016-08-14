bf81dfb2078605e6d7a9a2f32e0fe8fbc98fe9bc
crystalDf <chendong333@gmail.com>
2016-08-14 11:49:00 +0800

    Chapter 12 Excel

--- excel_test.py
+++ excel_test.py
@@ -1,5 +1,8 @@
 import openpyxl
 
+from openpyxl.cell import get_column_letter
+from openpyxl.cell import column_index_from_string
+
 wb = openpyxl.load_workbook('example.xlsx')
 print(type(wb))
 
@@ -38,3 +41,12 @@ for i in range(1, 8, 2):
 
 print(sheet.max_row)
 print(sheet.max_column)
+
+print(get_column_letter(1))
+print(get_column_letter(2))
+print(get_column_letter(27))
+print(get_column_letter(900))
+
+print(get_column_letter(sheet.max_column))
+print(column_index_from_string('A'))
+print(column_index_from_string('AA'))
